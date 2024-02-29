fa8f29e14b8c91852c9a7c74fce2547c504306ff
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-02-29 11:03:53 +0530

    Update main.cpp

--- mini/main.cpp
+++ mini/main.cpp
@@ -6,7 +6,6 @@ char board[3][3] = {{' ', ' ', ' '},
 
 void printBoad()
 {
-    int postiton;
     std::cout << std::endl
               << " -------------" << std::endl;
     for (int i = 0; i < 3; i++)
@@ -33,20 +32,42 @@ bool updateboard()
 
 bool checkWinner()
 {
-    int winnerCounter = 1;
+    int winnerCounterX = 1;
+    int winnerCounterY = 1;
+
+    // Horizontal check
     for (int i = 0; i < 3; i++)
     {
-        for (int j = i; j < 9; j += 3)
+        winnerCounterY = 1;
+        for (int j = 0; j < 3; j++)
         {
-            if(board[i][j] == 'x')
-            {winnerCounter++;}
+            if (board[i][j] == 'x')
+                winnerCounterY++;
         }
     }
-    std::cout << "winner count "<<winnerCounter <<std::endl;
-    if (winnerCounter >= 3){
-        return true;
-    }else{return false;}
+
+    // Vertical check
+    for (int i = 0; i < 3; i++)
+    {
+        winnerCounterX = 1;
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[j][i] == 'x')
+                winnerCounterX++;
+        }
+    }
+
+    // Diagonal check
+    if (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x')
+        winnerCounterX++;
+    if (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == 'x')
+        winnerCounterX++;
+
+    std::cout << "Winner count: " << winnerCounterX << std::endl;
+
+    return (winnerCounterX >= 3 || winnerCounterY >= 3);
 }
+
 
 int main()
 {
@@ -56,18 +77,18 @@ int main()
     {
         system("cls");
         printBoad();
-        if (winnerX )
+        if (winnerX)
         {
             break;
         }
         winnerX = checkWinner();
-        
+
         runningLoop = updateboard();
-        
     }
-    
-    if (winnerX){
-        std::cout<<"X person won this Game !!"<<std::endl;
+
+    if (winnerX)
+    {
+        std::cout << "X person won this Game !!" << std::endl;
     }
 
     std::cout << "Thanks for playing this game!!" << std::endl;
