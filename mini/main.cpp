2244bb865ed7dd3a0604cd78f2da9978457084c7
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-03-03 11:28:45 +0530

    Update main.cpp
    In this I have mange to somehow able run check for winner in this but not optimize but have problem in the main function. I will rewrite main function again.

--- mini/main.cpp
+++ mini/main.cpp
@@ -4,16 +4,17 @@ char board[3][3] = {{' ', ' ', ' '},
                     {' ', ' ', ' '},
                     {' ', ' ', ' '}};
 
-std::string getSpace(char sign ,const int times,char preSign = ' ', char postSign = ' ')
+std::string getSpace(char sign, const int times, char preSign = ' ', char postSign = ' ')
 {
     std::string space;
-    //Adding the precharacater here 
+    // Adding the precharacater here
     space += preSign;
-    //Filling the rest with symbol in string
-    for(int i = 0  ; i  < times ; i++){
+    // Filling the rest with symbol in string
+    for (int i = 0; i < times; i++)
+    {
         space += sign;
     }
-    //Adding the postcahracter here
+    // Adding the postcahracter here
     space += postSign;
     return space;
 }
@@ -21,9 +22,9 @@ std::string getSpace(char sign ,const int times,char preSign = ' ', char postSig
 void printBoad()
 {
 
-    std::cout<<getSpace(' ',13)<<"TIC TAC TOE"<<std::endl;
+    std::cout << getSpace(' ', 13) << "TIC TAC TOE" << std::endl;
     std::cout << std::endl
-              <<getSpace('-',13)<< std::endl;
+              << getSpace('-', 13) << std::endl;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -31,7 +32,7 @@ void printBoad()
             std::cout << " | " << board[i][j];
         }
         std::cout << " | " << std::endl
-                  <<getSpace('-',13)<< std::endl;
+                  << getSpace('-', 13) << std::endl;
     }
 }
 
@@ -43,55 +44,92 @@ bool updateboard(char swap_symbol, std::string println)
     std::cout << println;
     std::cin >> posx >> posy;
     board[posx - 1][posy - 1] = swap_symbol;
-    
+
     return ((posx > 0 && posx < 4) && (posy > 0 && posy < 4)) ? true : false;
 }
-
-bool checkWinner()
+// pending winner checking function
+char checkWinner()
 {
-    int winnerCounterX = 1;
-    int winnerCounterY = 1;
-
-    // Horizontal check
+    // horizatal check for winner
+    int winnerO = 0;
+    int winnerX = 0;
     for (int i = 0; i < 3; i++)
     {
-        winnerCounterY = 1;
+        if (winnerO >= 3 || winnerX >= 3)
+        {
+            break;
+        }
+        winnerO = 0;
+        winnerX = 0;
         for (int j = 0; j < 3; j++)
         {
-            if (board[i][j] == 'x')
-                winnerCounterY++;
+            if (board[j][i] == 'X')
+            {
+                winnerX++;
+            }
+            if (board[j][i] == 'O')
+            {
+                winnerO++;
+            }   
         }
+        if (winnerX == 3)
+            {
+                return 'X';
+            }
+        if (winnerO == 3)
+            {
+                return 'O';
+            }
     }
-
-    // Vertical check
+    
+    // vertcile check for winner
     for (int i = 0; i < 3; i++)
     {
-        winnerCounterX = 1;
+        if (winnerO >= 3 || winnerX >= 3)
+        {
+            break;
+        }
+        winnerO = 0;
+        winnerX = 0;
         for (int j = 0; j < 3; j++)
         {
-            if (board[j][i] == 'x')
-                winnerCounterX++;
+            if (board[i][j] == 'X')
+            {
+                winnerX++;
+            }
+            if (board[i][j] == 'O')
+            {
+                winnerO++;
+            }
         }
+        if (winnerX == 3)
+            {
+                return 'X';
+            }
+            if (winnerO == 3)
+            {
+                return 'O';
+            }
     }
+     // Diagonal check for winner
+    if ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+        (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'))
+        return 'X';
+    if ((board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') ||
+        (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O'))
+        return 'O';
 
-    // Diagonal check
-    if (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x')
-        winnerCounterX++;
-    if (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == 'x')
-        winnerCounterX++;
 
-    std::cout << "Winner count: " << winnerCounterX << std::endl;
-
-    return (winnerCounterX >= 3 || winnerCounterY >= 3);
+    return '!';
 }
+
 
 int main()
 {
     bool runningLoop = true;
-    bool winnerX = false;
-    bool winnerO = false;
+    char winner = '!';
     char OPTION = 'n';
-
+    
     system("cls");
     std::cout << "YOU WANT TO PLAY IT ALONE (y/n)?    <y is not complete dont use it>\n";
     std::cin >> OPTION;
@@ -101,23 +139,24 @@ int main()
         if (OPTION == 'n' || OPTION == 'N')
         {
             system("cls");
-            if (winnerX || winnerO)
+           
+            winner = checkWinner();
+            runningLoop = (updateboard('O', "Enter coordination for O : ") && updateboard('X', "Enter coordination for X : "));
+            
+            if (winner == 'O'||winner == 'X')
             {
                 break;
             }
-            winnerX = checkWinner();
-
-            runningLoop = (updateboard('O', "Enter coordination for O : ") && updateboard('X', "Enter coordination for X : "));
         }
     }
 
-    if (winnerX)
+    if (winner == 'X')
     {
-        std::cout << "X person won this Game !!" << std::endl;
+        std::cout << "X won this Game !!" << std::endl;
     }
-    if (winnerO)
+    else if (winner == 'O')
     {
-        std::cout << "O person won this Game !!" << std::endl;
+        std::cout << "O won this Game !!" << std::endl;
     }
 
     std::cout << "\nThanks for playing this game!!" << std::endl;
